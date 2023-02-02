0112933479f2b30d23bd75c062a7274a9e0d04b9
Mehmet <unal.m1991@gmail.com>
2023-02-02 20:34:17 +0300

    IsEmpty

--- src/Libraries/Dev.Core/Extensions/EnumerableExtensions.cs
+++ src/Libraries/Dev.Core/Extensions/EnumerableExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Dev.Core.Model;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -70,6 +71,16 @@ namespace Dev.Core.Extensions
             }
 
             return q;
+        }
+
+        public static bool IsEmpty<TSource>(this IEnumerable<TSource> source)
+        {
+            return source != null && source.Any();
+        }
+
+        public static bool IsEmpty(this IEnumerable source)
+        {
+            return source != null;
         }
     }
 }
