73ef5e61da5af380f143262e99e902f7574534f9
Mehmet <unal.m1991@gmail.com>
2023-10-28 23:42:41 +0300

    fix

---
+++ src/Libraries/Maggsoft.Core/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Maggsoft.Core.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime Next(this DateTime from, DayOfWeek dayOfWeek)
+        {
+            int start = (int)from.DayOfWeek;
+            int target = (int)dayOfWeek;
+            if (target <= start)
+                target += 7;
+            return from.AddDays(target - start);
+        }
+    }
+}
