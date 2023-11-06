0db6d255273965a6abb3d2eadcd0003738f5faad
Mehmet <unal.m1991@gmail.com>
2023-11-06 14:37:51 +0300

    To Slug

--- src/Libraries/Maggsoft.Core/Extensions/StringExtensions.cs
+++ src/Libraries/Maggsoft.Core/Extensions/StringExtensions.cs
@@ -764,6 +764,28 @@ namespace Maggsoft.Core.Extensions
             return input;
         }
 
+        public static string ToSlug(this string name)
+        {
+            var phrase = string.Format("{0}", name);
+            var str = RemoveAccent(phrase).ToLower();
+            str = RemoveAccent(str).ToLower();
+            // invalid chars           
+            str = Regex.Replace(str, @"[^a-z0-9\s-]", "");
+            // convert multiple spaces into one space   
+            str = Regex.Replace(str, @"\s+", " ").Trim();
+            // cut and trim 
+            str = str.Substring(0, str.Length).Trim();
+            str = Regex.Replace(str, @"\s", "-"); // hyphens   
+            str = str.Replace(".", "-");
+            return str;
+        }
+       
+        private static string RemoveAccent(string text)
+        {
+            byte[] bytes = Encoding.GetEncoding("Cyrillic").GetBytes(text);
+            return Encoding.ASCII.GetString(bytes);
+        }
+
         #endregion
 
     }
