86494828036512dd3e244c06bb7cdffcf13cadf9
Mehmet <unal.m1991@gmail.com>
2023-12-06 16:51:07 +0300

    QueryableExtensions

---
+++ src/Libraries/Maggsoft.Core/Extensions/QueryableExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Maggsoft.Core.Extensions
+{
+    public static class QueryableExtensions
+    {
+        public static IQueryable<T> WhereIf<T>(
+            this IQueryable<T> queryable,
+            bool condition,
+            Expression<Func<T,bool>> predicate)
+        {
+            if (condition)
+            {
+                queryable = queryable.Where(predicate);
+            }
+
+            return queryable;
+        }
+    }
+}
