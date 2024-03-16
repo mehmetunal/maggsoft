5b5040e46de470f674bd3a14f76b67a015b0c893
Mehmet <unal.m1991@gmail.com>
2024-03-16 14:14:32 +0300

    fix

---
+++ src/Libraries/Data/Maggsoft.Data.Sqlite/Localization/Language.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Maggsoft.Data.Sqlite.Localization;
+
+public partial class Language : BaseEntity, IPrimaryKey<Guid>
+{
+    public Language()
+    {
+        LocaleStringResources = new List<LocaleStringResource>();
+    }
+
+    [Required]
+    [Column(Order = 8)]
+    /// <summary>
+    /// Gets or sets the name
+    /// </summary>
+    public string Name { get; set; }
+
+    [Required]
+    [Column(Order = 9)]
+    /// <summary>
+    /// Gets or sets the language culture
+    /// </summary>
+    public string LanguageCulture { get; set; }
+
+    [Required]
+    [Column(Order = 10)]
+    /// <summary>
+    /// Gets or sets the unique SEO code
+    /// </summary>
+    public string UniqueSeoCode { get; set; }
+
+    [Required]
+    [Column(Order = 11)]
+    /// <summary>
+    /// Gets or sets the flag image file name
+    /// </summary>
+    public string FlagImageFileName { get; set; }
+
+    [DefaultValue(false)]
+    [Column(Order = 12)]
+    /// <summary>
+    /// Gets or sets a value indicating whether the language supports "Right-to-left"
+    /// </summary>
+    public bool Rtl { get; set; }
+
+    public virtual ICollection<LocaleStringResource> LocaleStringResources { get; set; }
+}
