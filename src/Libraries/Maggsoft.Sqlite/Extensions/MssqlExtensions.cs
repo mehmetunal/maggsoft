aa60c9714d0808b7b3d594b10437801dba304742
Mehmet <unal.m1991@gmail.com>
2024-03-16 14:38:30 +0300

    fix

--- src/Libraries/Maggsoft.Sqlite/Extensions/MssqlExtensions.cs
+++ src/Libraries/Maggsoft.Sqlite/Extensions/MssqlExtensions.cs
@@ -1,25 +1,17 @@
 ﻿using FluentMigrator;
 using FluentMigrator.Runner;
-using Maggsoft.Data.DataProviders;
 using Maggsoft.Data.Migration;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Data.Common;
 using System.Linq;
-using System.Threading;
 
 namespace Maggsoft.Sqlite.Extensions;
 
-public static class MssqlExtensions
+public static class SqliteExtensions
 {
-    public static IServiceCollection AddMssqlConfig<TContext>(this IServiceCollection services,
+    public static IServiceCollection AddSqliteConfig<TContext>(this IServiceCollection services,
         IConfiguration configuration) where TContext : DbContext
     {
         var connection = configuration.GetConnectionString("DefaultConnection");
@@ -43,7 +35,7 @@ public static class MssqlExtensions
             .AddFluentMigratorCore()
             .ConfigureRunner(builder =>
             {
-                builder.AddSqlServer();
+                builder.AddSQLite();
                 builder.WithGlobalConnectionString(connection);
                 builder.ScanIn(mAssemblies).For.Migrations();
             })
@@ -53,134 +45,5 @@ public static class MssqlExtensions
         services.AddScoped<IMigrationManager, MigrationManager>();
 
         return services;
-    }
-    // <summary>
-    // 
-    // </summary>
-    // <param name = "app" ></ param >
-    // < typeparam name="TContext"></typeparam>
-    // <returns></returns>
-    public static IApplicationBuilder AddMigrateConfigure<TContext>(this IApplicationBuilder app) where TContext : DbContext
-    {
-        using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
-        {
-            var context = serviceScope.ServiceProvider.GetRequiredService<TContext>();
-            context.Database.EnsureCreated();
-            //context.Database.Migrate();
-        }
-
-        return app;
-    }
-
-    /// <summary>  
-    /// Migrates the database.  
-    /// </summary>  
-    /// <typeparam name="T"></typeparam>  
-    /// <param name="host">The web host.</param>  
-    /// <returns>IWebHost.</returns>  
-    public static IHost CreateDatabase<TContext>(this IHost host) where TContext : DbContext
-    {
-        using var scope = host.Services.CreateScope();
-        var services = scope.ServiceProvider;
-        var logger = services.GetRequiredService<ILogger<TContext>>();
-        try
-        {
-            var context = services.GetRequiredService<TContext>();
-            RelationalDatabaseCreator databaseCreator = (RelationalDatabaseCreator)context.Database.GetService<IDatabaseCreator>();
-            if (!databaseCreator.Exists())
-            {
-                bool ensureCreated = context.Database.EnsureCreated();
-                //databaseCreator.CreateTables();
-                //context.Database.Migrate();
-            }
-            Console.WriteLine("Database migration completed.");
-            logger.LogInformation("Database migration completed.");
-        }
-        catch (Exception ex)
-        {
-            logger.LogError(ex, "An error occurred migrate the DB.");
-        }
-
-        return host;
-    }
-
-    /// <summary>  
-    /// Migrates the database.  
-    /// </summary>  
-    /// <typeparam name="T"></typeparam>  
-    /// <param name="host">The web host.</param>  
-    /// <returns>IWebHost.</returns>  
-    public static IHost CreateDatabase(this IHost host, int triesToConnect = 10)
-    {
-        if (DatabaseExists(host))
-            return host;
-
-        var builder = GetConnectionStringBuilder(host);
-
-        //gets database name
-        var databaseName = builder.InitialCatalog;
-
-        //now create connection string to 'master' dabatase. It always exists.
-        builder.InitialCatalog = "master";
-
-        using (var connection = GetInternalDbConnection(builder.ConnectionString))
-        {
-            var query = $"CREATE DATABASE [{databaseName}]";
-
-            var command = connection.CreateCommand();
-            command.CommandText = query;
-            command.Connection.Open();
-
-            command.ExecuteNonQuery();
-        }
-
-        //try connect
-        if (triesToConnect <= 0)
-            return host;
-
-        //sometimes on slow servers (hosting) there could be situations when database requires some time to be created.
-        //but we have already started creation of tables and sample data.
-        //as a result there is an exception thrown and the installation process cannot continue.
-        //that's why we are in a cycle of "triesToConnect" times trying to connect to a database with a delay of one second.
-        for (var i = 0; i <= triesToConnect; i++)
-        {
-            if (i == triesToConnect)
-                throw new Exception("Unable to connect to the new database. Please try one more time");
-
-            if (!DatabaseExists(host))
-                Thread.Sleep(1000);
-            else
-                break;
-        }
-
-        return host;
-    }
-    /// <summary>
-    /// Checks if the specified database exists, returns true if database exists
-    /// </summary>
-    /// <param name="host">IHost</param>
-    /// <returns>Returns true if the database exists.</returns>
-    private static bool DatabaseExists(IHost host)
-    {
-        try
-        {
-            using var context = GetInternalDbConnection(DataProviderExtensions.GetCurrentConnectionString(host));
-            context.Open();
-            return true;
-        }
-        catch
-        {
-            return false;
-        }
-    }
-
-    private static Microsoft.Data.SqlClient.SqlConnectionStringBuilder GetConnectionStringBuilder(IHost host)
-    {
-        return new Microsoft.Data.SqlClient.SqlConnectionStringBuilder(DataProviderExtensions.GetCurrentConnectionString(host));
-    }
-
-    private static DbConnection GetInternalDbConnection(string connectionString)
-    {
-        return new Microsoft.Data.SqlClient.SqlConnection(connectionString);
     }
 }
