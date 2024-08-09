f8320b525cf617a26ac21df89fb0709fa8bdd9a5
Mehmet <unal.m1991@gmail.com>
2024-08-09 16:58:13 +0300

    fix

--- src/Libraries/Maggsoft.Core/Messages/EmailAccountSettings.cs
+++ src/Libraries/Maggsoft.Core/Messages/EmailAccountSettings.cs
@@ -8,5 +8,5 @@ public class EmailAccountSettings
     /// <summary>
     /// Gets or sets a store default email account identifier
     /// </summary>
-    public int DefaultEmailAccountId { get; set; }
+    public object DefaultEmailAccountId { get; set; }
 }
