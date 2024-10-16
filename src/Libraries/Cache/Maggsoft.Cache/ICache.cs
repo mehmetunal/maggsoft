3c7fec423bfa12d2f00c5c06f3c84912d4efbcc5
Mehmet <unal.m1991@gmail.com>
2024-10-16 13:33:28 +0300

    f

--- src/Libraries/Cache/Maggsoft.Cache/ICache.cs
+++ src/Libraries/Cache/Maggsoft.Cache/ICache.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 
 namespace Maggsoft.Cache;
  
-public interface ICache  
+public interface ICache     
 {
     object Get(string cacheKey);
     object Get(string cacheKey, Type deserializeType);
