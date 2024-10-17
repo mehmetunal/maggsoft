8c3ec5a17c2a71a05310173ebf23f901e81d9579
Mehmet <unal.m1991@gmail.com>
2024-10-17 17:14:59 +0300

    version veren proje oluşturuldu

---
+++ src/Tools/VersioningTool/Program.cs
@@ -0,0 +1,43 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string pathName = args.Length > 0 ? args[0] : "";
+        string versionFilePath = $"VERSION{(!string.IsNullOrEmpty(pathName) ? $"_{pathName}" : "")}.txt";
+
+        string directoryPath = $"{Directory.GetCurrentDirectory()}/version_folder";
+        string filePath = Path.Combine(directoryPath, versionFilePath);
+
+        string currentVersion = "1.0.0";
+        if (File.Exists(filePath))
+        {
+            currentVersion = File.ReadAllText(filePath).Trim();
+        }
+
+        if (!Directory.Exists(directoryPath))
+        {
+            Directory.CreateDirectory(directoryPath);
+        }
+
+
+        using (File.Create(filePath))
+        {
+            // Dosyaya yazma işlemleri buraya eklenebilir.
+        }
+
+
+        string[] versionParts = currentVersion.Split('.');
+        int major = int.Parse(versionParts[0]);
+        int minor = int.Parse(versionParts[1]);
+        int patch = int.Parse(versionParts[2]);
+
+        patch++; // Patch versiyonunu artır
+
+        string newVersion = $"{major}.{minor}.{patch}";
+
+        File.WriteAllText(filePath, newVersion);
+
+        Console.WriteLine(newVersion);
+
+    }
+}
