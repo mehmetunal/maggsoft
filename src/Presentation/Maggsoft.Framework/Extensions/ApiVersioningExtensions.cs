69b47ada3e0c26e554a88e1634da2ff9cd061a3c
Mehmet <unal.m1991@gmail.com>
2024-10-21 12:57:14 +0300

    change fix

--- src/Presentation/Maggsoft.Framework/Extensions/ApiVersioningExtensions.cs
+++ src/Presentation/Maggsoft.Framework/Extensions/ApiVersioningExtensions.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Maggsoft.Framework.Extensions;
 
-public static class ApiVersioningExtensions
+public static class ApiVersioningExtensions 
 {
     /// <summary>
     /// AddApiVersioningConfig
